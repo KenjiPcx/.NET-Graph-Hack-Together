a289fd0be5f41aa99cae600aa6304c4d4d7e600b
Kenji Phang <ken.pcx@outlook.com>
2023-03-07 04:23:54 +0000

    Add migrations programatically for during startup

--- dotnet-blazor-server-app-microsoft-graph/Program.cs
+++ dotnet-blazor-server-app-microsoft-graph/Program.cs
@@ -41,6 +41,15 @@ builder.Services.AddOpenAIService();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<GraphassDbContext>();
+
+    // Ensure that the database is migrated to the latest version
+    await dbContext.Database.EnsureCreatedAsync();
+    await dbContext.Database.MigrateAsync();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
